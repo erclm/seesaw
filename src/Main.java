bc2c9ea6dcd11cf392dbe4824fce0c75aca5aba0
Eric Lim <jilim@chapman.edu>
2019-11-04 20:01:37 -0800

    did not work

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,6 @@ import java.util.concurrent.Semaphore;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Person willma = new Person("willma", 1.5, 7, false);
-        Person fred = new Person("fred", 1.0, 1, true);
-        Semaphore s1 = new Semaphore(1);
-        Semaphore s2 = new Semaphore(1);
-
-        compute t1 = new compute();
-        compute t2 = new compute();
-
-            try {
-                t1.run(fred, willma, s1, s2);
-                t1.wait(1000);
-                t2.run(willma, fred, s2, s1);
-                t2.wait(1000);
-
-
-                t1.join();
-                t2.join();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
 
     }
 }
